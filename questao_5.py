aafb430455733d6efa958849b86545a8bb4cd077
Breno Igo <brenoigo@gmail.com>
2026-04-26 01:22:40 -0300

    Adicionando código principal do sistema de senhas (Questão 05)

--- questao_5.py
+++ questao_5.py
@@ -1,1 +1,26 @@
+import seguranca
 
+print('PIAUÍ INSTITUTO DE TECNOLOGIA - PIT')
+print('Bacharelado em Inteligência Artificial')
+print()
+print('VERIFICAÇÃO DE SENHA')
+print()
+
+while True:
+  senha_digitada = input('Colaborador, digite sua senha (mínimo de 8 caracteres, necessário pelo menos um número e uma letra maiúscula): ')
+
+  status, mensagem = seguranca.verificar_senha(senha_digitada)
+
+  if status == True:
+    print()
+    print(f'{mensagem}')
+    break
+  elif status == False:
+    print()
+    print(f'{mensagem}')
+    print('Por favor, tente novamente')
+  else:
+    print()
+    print('Algo deu errado!')
+    print('Por favor, tente novamente')
+    break
